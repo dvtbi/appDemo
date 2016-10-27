66ff66e9f234355e3db92f88b09fff2598dad103
NickSyl <sunyuliang914@sina.com>
2016-10-27 16:58:10 +0800

    添加分析页面

--- DVT.SELFBIAPPDEMO/www/js/controller/searchCtrl.js
+++ DVT.SELFBIAPPDEMO/www/js/controller/searchCtrl.js
@@ -1,6 +1,11 @@
 app.controller('searchCtrl',['$scope','$state','$stateParams',function($scope,$state,$stateParams){
 	$scope.query=$stateParams.query;
-	$scope.goback=function(event){
-		$state.go('home',{query:$scope.query});
+	$scope.goback=function(event){	
+		if ($scope.query=='a') {
+			$state.go('chart',{query:$scope.query});
+		}else{
+		$state.go('home', { query: $scope.query });			
+		}
 	}
+
 }]);
