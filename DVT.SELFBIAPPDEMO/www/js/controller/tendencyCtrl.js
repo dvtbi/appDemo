8dde84686ba223f3be1e8e5606453a98bc40b776
NickSyl <sunyuliang914@sina.com>
2016-11-01 17:08:23 +0800

    图片基本处理

--- DVT.SELFBIAPPDEMO/www/js/controller/tendencyCtrl.js
+++ DVT.SELFBIAPPDEMO/www/js/controller/tendencyCtrl.js
@@ -1,118 +1,104 @@
 app.controller('tendencyCtrl', ['$scope', '$state', '$stateParams', function($scope, $state, $stateParams) {
-    $scope.name = "test";
-    // 基于准备好的dom，初始化echarts实例
-    var myChart = echarts.init(document.getElementById('main'));
 
+  
+    $scope.goback=function(event){
+        $state.go('home',{query:$scope.IndicatorName});
+    };
+    $scope.goProduce=function(i,n){
+         $state.go('home',{query:$scope.IndicatorName});
+    }
+
+    $scope.IndicatorName="趋势分析图";
+      // 基于准备好的dom，初始化echarts实例
+    var myChart = echarts.init(document.getElementById('main'));
     // 指定图表的配置项和数据
     option = {
-        title: {
-            text: '堆叠区域图'
-        },
-        tooltip: {
-            trigger: 'axis'
-        },
-        legend: {
-            data: []
-        },
-        toolbox: {
-            feature: {
-                saveAsImage: {}
+    title: {
+        text: '发电量',
+        subtext: ''
+    },
+    tooltip: {
+        trigger: 'axis'
+    },
+    legend: {
+        data:['发电量','去年同期']
+    },
+    toolbox: {
+        show: false,
+        feature: {
+            dataZoom: {
+                yAxisIndex: 'none'
+            },
+            dataView: {readOnly: false},
+            magicType: {type: ['line', 'bar']},
+            restore: {},
+            saveAsImage: {}
+        }
+    },
+    xAxis:  {
+        type: 'category',
+        boundaryGap: false,
+        data: ['10.2','10.3','10.4','10.5','10.6','10.7','10.8',"10.9"]
+    },
+    yAxis: {
+        type: 'value',
+        axisLabel: {
+            formatter: '{value} kw/h'
+        }
+    },
+    series: [
+        {
+            name:'发电量',
+            type:'line',
+            data:[11, 11, 15, 13, 12,19,24,26],
+            markPoint: {
+                data: [
+                    {type: 'max', name: '最大值'},
+                    {type: 'min', name: '最小值'}
+                ]
+            },
+            markLine: {
+                data: [
+                    {type: 'average', name: '平均值'}
+                ]
             }
         },
-        grid: {
-            left: '3%',
-            right: '4%',
-            bottom: '3%',
-            containLabel: true
-        },
-        xAxis: [{
-            type: 'category',
-            boundaryGap: false,
-            data: ['周一', '周二', '周三', '周四', '周五', '周六', '周日']
-        }],
-        yAxis: [{
-            type: 'value'
-        }],
-        series: [{
-            name: '邮件营销',
-            type: 'line',
-            stack: '总量',
-            areaStyle: { normal: {} },
-            data: [120, 132, 101, 134, 90, 230, 210]
-        }, {
-            name: '联盟广告',
-            type: 'line',
-            stack: '总量',
-            areaStyle: { normal: {} },
-            data: [220, 182, 191, 234, 290, 330, 310]
-        }, {
-            name: '视频广告',
-            type: 'line',
-            stack: '总量',
-            areaStyle: { normal: {} },
-            data: [150, 232, 201, 154, 190, 330, 410]
-        }, {
-            name: '直接访问',
-            type: 'line',
-            stack: '总量',
-            areaStyle: { normal: {} },
-            data: [320, 332, 301, 334, 390, 330, 320]
-        }, {
-            name: '搜索引擎',
-            type: 'line',
-            stack: '总量',
-            label: {
-                normal: {
-                    show: true,
-                    position: 'top'
-                }
+        {
+            name:'去年同期',
+            type:'line',
+            data:[10, 9, 12, 20, 22,],
+            markPoint: {
+                data: [
+                    {name: '周最低', value: -2, xAxis: 1, yAxis: -1.5}
+                ]
             },
-            areaStyle: { normal: {} },
-            data: [820, 932, 901, 934, 1290, 1330, 1320]
-        }]
-    };
+            markLine: {
+                data: [
+                    {type: 'average', name: '平均值'},
+                    [{
+                        symbol: 'none',
+                        x: '90%',
+                        yAxis: 'max'
+                    }, {
+                        symbol: 'circle',
+                        label: {
+                            normal: {
+                                position: 'start',
+                                formatter: '最大值'
+                            }
+                        },
+                        type: 'max',
+                        name: '最高点'
+                    }]
+                ]
+            }
+        }
+    ]
+};
 
 
     // 使用刚指定的配置项和数据显示图表。
     myChart.setOption(option);
 
-    var myChart2 = echarts.init(document.getElementById('main2'));
-    option2 = {
-        title: {
-            text: '发电量',
-            subtext: '纯属虚构',
-            x: 'center'
-        },
-        tooltip: {
-            trigger: 'item',
-            formatter: "{a} <br/>{b} : {c} ({d}%)"
-        },
-        legend: {
-            orient: 'vertical',
-            left: 'left',
-            data: ['直接访问', '邮件营销', '联盟广告', '视频广告', '搜索引擎']
-        },
-        series: [{
-            name: '访问来源',
-            type: 'pie',
-            radius: '55%',
-            center: ['50%', '60%'],
-            data: [
-                { value: 335, name: '直接访问' },
-                { value: 310, name: '邮件营销' },
-                { value: 234, name: '联盟广告' },
-                { value: 135, name: '视频广告' },
-                { value: 1548, name: '搜索引擎' }
-            ],
-            itemStyle: {
-                emphasis: {
-                    shadowBlur: 10,
-                    shadowOffsetX: 0,
-                    shadowColor: 'rgba(0, 0, 0, 0.5)'
-                }
-            }
-        }]
-    };
-
-    myChart2.setOption(option2);
+   
 }])
